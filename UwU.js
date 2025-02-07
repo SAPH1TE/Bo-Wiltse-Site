772251623fd870bb1c8538be2d95b0430bcb0ed8
SAPH1TE <sssaphite@gmail.com>
2025-02-06 16:49:15 -0800

    Update UwU.js

--- UwU.js
+++ UwU.js
@@ -1,15 +1,1 @@
-    (function () {
-        const konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-        let codePosition = 0;
-
-        document.addEventListener('keydown', function (e) {
-            if (e.keyCode === konamiCode[codePosition]) {
-                codePosition++;
-                if (codePosition === konamiCode.length) {
-                    window.location.href = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Transgender_Pride_flag.svg/640px-Transgender_Pride_flag.svg.png'; // Change this URL to the target website
-                }
-            } else {
-                codePosition = 0;
-            }
-        });
-    })();
+//nothing was here 100% for sure *Tehe*
