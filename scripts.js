fa217907ff2d59493e7e5317089daaa08df02184
SAPH1TE <sssaphite@gmail.com>
2025-02-13 16:25:32 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,42 @@
+    (function () {
+        const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+        let codePosition = 0;
 
+        document.addEventListener('keydown', function (e) {
+            if (e.key === konamiCode[codePosition]) {
+                codePosition++;
+                if (codePosition === konamiCode.length) {
+                    window.location.href = 'https://github.com/SAPH1TE/Bo-Wiltse-Site/blob/main/vexation.png?raw=true';
+                    codePosition = 0;
+                }
+            } else {
+                codePosition = 0;
+            }
+        });
+    })();
+
+    (() => {
+        let d = document,
+            b = d.createElement("div");
+        b.textContent = "Please note this website is in development";
+        Object.assign(b.style, {
+            position: "fixed",
+            bottom: "25px",
+            right: "25px",
+            background: "black",
+            color: "white",
+            padding: "8px",
+            borderRadius: "15px",
+            fontSize: "25px",
+            zIndex: 9999
+        });
+        d.body.appendChild(b);
+        setTimeout(() => b.remove(), 2500);
+    })();
+
+    window.addEventListener('load', function () {
+        document.body.classList.add('loaded');
+        setTimeout(function () {
+            document.getElementById('loading-section').remove();
+        }, 500);
+    });
